9b7f297e4932925921ea9535ba3da3e7a6bb5ace
Josh Curry <joshuacurry2615@gmail.com>
2020-06-24 15:04:06 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Form({onSubmit}){
+  const [userName, setUserName] = useState('');
+  async function handleSubmit(event){
+    event.preventDefault();
+    const resp = await axios.get(`https://api.github.com/users/${userName}`)
+    onSubmit(resp.data);
+    setUserName('');
+  }
+  
+  return(
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        value={userName}
+        onChange={(event) => setUserName(event.target.value)}
+        placeholder="GitHub username" 
+        required
+      />
+      <button>Add card</button>
+    </form>
+  )
+}
+
+function CardList({profiles}){
+  return(
+    <div>
+      {profiles.map(profile => <Card key={profile.id} {...profile}/>)}
     </div>
-  );
+  )
+}
+
+function Card(props){
+  const profile = props;
+  return(
+    <div className="github-profile">
+      <img alt="Avatar of user" src={profile.avatar_url}/>
+      <div className="info">
+        <div className="name">{profile.name}</div>
+        <div className="company">{profile.company}</div>
+      </div>
+    </div>
+  )
+}
+
+function App({title}){
+  const [profiles, setProfiles] = useState([]);
+  function addNewProfile(profileData){
+    setProfiles([...profiles, profileData])
+  }
+  return(
+    <div>
+      <div className="header">{title}</div>
+      <Form onSubmit={addNewProfile}/>
+      <CardList profiles={profiles}/>
+    </div>
+  )
 }
 
 export default App;
